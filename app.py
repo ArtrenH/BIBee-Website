4e39b259161db5c1a179d29c9d1518addee7b0a4
ArtrenH <79058115+ArtrenH@users.noreply.github.com>
2024-10-07 23:38:24 +0200

    feat: massive update

--- app.py
+++ app.py
@@ -1,10 +1,66 @@
-from flask import Flask, render_template
+import os
+import json
+from dotenv import load_dotenv
+from flask import Flask, render_template, request, jsonify
+import requests
+
+load_dotenv()
 
 app = Flask(__name__)
+app.template_folder = 'templates'
+app.static_folder = 'static'
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
+@app.route('/register', methods=['POST'])
+def register():
+    # Get form data
+    name = request.form['name']
+    email = request.form['email']
+    role = request.form['role']
+
+    # Log the registration in a file (json format)
+    registration = {
+        "name": name,
+        "email": email,
+        "roll": role
+    }
+    with open('registrations.log', 'a') as f:
+        json.dump(registration, f)
+        f.write('\n')
+
+    # Send data to Discord webhook
+    try:
+        webhook_url = os.getenv('WEBHOOK_URL')
+        discord_data = {
+            "embeds": [{
+                "title": "Neue Registrierung für die Bonn Integration Bee",
+                "fields": [
+                    {
+                        "name": "Name",
+                        "value": name
+                    },
+                    {
+                        "name": "E-Mail",
+                        "value": email
+                    },
+                    {
+                        "name": "Rolle",
+                        "value": role
+                    }
+                ]
+            }]
+        }
+        requests.post(webhook_url, json=discord_data)
+    except Exception:
+        return jsonify({"message": "Webhook ging nicht, Rest schon"})
+
+    return jsonify({"message": "Erfolgreich registriert!"})
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=3001)
